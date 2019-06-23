1416016b1ef7edbb4223ec69c754fc0e95fd24b4
Pinghai Yuan <pinghaiyuan@gmail.com>
2019-06-23 17:29:04 +0800

    Can identify array access  Changes to be committed: 	modified:   symbolicVSA/ArrayInfer.java 	modified:   unittests/TestArrayInfer.java

--- redev/symbolicVSA/ArrayInfer.java
+++ redev/symbolicVSA/ArrayInfer.java
@@ -1,5 +1,162 @@
+package symbolicVSA;
+
+import java.util.*;
+
+import com.sun.source.tree.Scope;
+
+import symbolicVSA.*;
+
 public class ArrayInfer {
+    private SymbolicCalculator m_calc;
+
     public ArrayInfer() {
-        
+        m_calc = SymbolicCalculator.getCalculator();
+    }
+
+    /**
+     * Find out all scopes: each pure symbolic value representing a new scope
+     * 
+     * @param mapSMAT
+     * @return
+     */
+    public Set<Map<String, List<Long>>> findArrayAccess(Map<Long, Map<String, Set<String>>> mapSMAT) {
+        Set<Map<String, List<Long>>> setArrayAccess = new HashSet<>();
+        Map<String, List<Long>> mapArrayAccess;
+        List<Long> listVS;
+        String scope, addr;
+
+        for (Map.Entry<Long, Map<String, Set<String>>> entMapSMAT : mapSMAT.entrySet()) {
+            Map<String, Set<String>> mapVS = entMapSMAT.getValue();
+            /* WIDENING_THRESHOLD == 4 */
+            if (mapVS.size() < 4)
+                continue;
+
+            List<String> listAddr = new ArrayList<>(mapVS.keySet());
+            assert (listAddr.size() > 0);
+            
+
+            /* Test if in a Scope ? */
+            addr = listAddr.get(0);
+            if (addr.length() < 1 || addr.charAt(0) != 'V')
+                continue;
+            /* Get the scope name */
+            scope = addr.split(" ", 0)[0];
+
+
+            /* In the same scope ? */
+            boolean bSameScope = true;
+            listVS = new ArrayList<>();
+            for (int i = 0; i < listAddr.size(); i++) {                
+                String delta = m_calc.symbolicSub(listAddr.get(i), scope);
+                if (!m_calc.isPureDigital(delta)) {
+                    bSameScope = false;
+                    break;
+                }
+                else {
+                    listVS.add(Long.decode(delta));
+                }
+            }
+            if (!bSameScope)
+                continue;
+
+            /* Find an array access */
+            mapArrayAccess = new HashMap<>();
+            Collections.sort(listVS);
+            mapArrayAccess.put(scope, listVS);
+            setArrayAccess.add(mapArrayAccess);
+        }
+
+        return setArrayAccess;
+    }
+
+    /**
+     * Find out all scopes: each pure symbolic value representing a new scope
+     * 
+     * @param mapSMAT
+     * @return
+     */
+    public Map<String, List<Long>> findScopeAccesssWOArray(Map<Long, Map<String, Set<String>>> mapSMAT) {
+        Map<String, List<Long>> mapScopeAccess = null;
+        List<Long> addrSet;
+        String scope;
+
+        for (Map.Entry<Long, Map<String, Set<String>>> entMapSMAT : mapSMAT.entrySet()) {
+            Map<String, Set<String>> mapVS = entMapSMAT.getValue();
+
+            if (mapVS.size() > 2)
+                continue; // ignore array access, at most one register and one memory operand
+
+            /* all stack address accessed by other instructions */
+            for (Map.Entry<String, Set<String>> entMapVS : mapVS.entrySet()) {
+                String addr = entMapVS.getKey();
+                assert (addr.length() > 0);
+                if (addr.charAt(0) != 'V')
+                    continue;
+
+                /* Get scope name */
+                scope = addr.split(" ", 0)[0];
+
+                if (mapScopeAccess == null) {
+                    mapScopeAccess = new HashMap<>();
+                }
+                addrSet = mapScopeAccess.get(scope);
+                if (addrSet == null) {
+                    addrSet = new ArrayList<>();
+                    mapScopeAccess.put(scope, addrSet);
+                }
+
+                /* The address may be: VRSP + VRAX + 100, so we need further verification */
+                String delta = m_calc.symbolicSub(addr, scope);
+                if (!m_calc.isPureDigital(delta))
+                    continue;
+
+
+                addrSet.add(Long.decode(delta));
+                Collections.sort(addrSet);
+            }
+        }
+        return mapScopeAccess;
+    }
+
+
+    public void inferArray(Set<Map<String, List<Long>>> setArrayAccess, Map<String, List<Long>> mapScopeAccess) {
+        List<Long> listScopeVS;
+
+        for (Map<String, List<Long>> mapArrayAccess : setArrayAccess) {
+            List<Long> listArrOffset = new ArrayList<>();;
+            String scope = "";
+
+            for (Map.Entry<String, List<Long>> entArrayAccess : mapArrayAccess.entrySet()) {
+                scope = entArrayAccess.getKey();
+                listArrOffset = entArrayAccess.getValue();
+                break; // Should have only one elment
+            }
+
+            // Collections.sort(listArrOffset);
+            long maxAddr = (long) Collections.max(listArrOffset);
+            long minAddr = (long) Collections.min(listArrOffset); // Base-addrss => Scope + minAddr
+            long stride = (long) listArrOffset.get(1) - (long) listArrOffset.get(0); // Stride
+
+            /* Calculate up-bound */
+            long upbound = maxAddr;
+
+            listScopeVS = mapScopeAccess.get(scope);
+            if (listScopeVS == null) {
+                upbound = maxAddr;
+            } else {
+                /* find max lowerbound */
+                // Collections.sort(listScopeVS); // in asending order
+                for (Long v : listScopeVS) {
+                    if (v > maxAddr) {
+                        upbound = v;
+                        break;
+                    }
+                }
+            }
+            /* For debuging */
+            String msg = String.format("Base: %s%d, stide: %d: upbound: %d", scope, minAddr, stride, upbound);
+            System.out.println(msg);
+
+        }
     }
 }
--- redev/unittests/TestArrayInfer.java
+++ redev/unittests/TestArrayInfer.java
@@ -23,18 +23,18 @@ class TestClass {
          */
 
         /**
-         * 0x400546 = {VRSP -8=[VRBP], RSP=[VRSP -8]} -------del----- 0x400547 =
-         * {RBP=[VRSP -8]} -------del----- 0x40054a = {RSP=[VRSP -136]} 0x40054e = {VRSP
+         * 0x400546 = {VRSP -8=[VRBP], RSP=[VRSP -8]} -------ignore----- 0x400547 =
+         * {RBP=[VRSP -8]} -------ignore----- 0x40054a = {RSP=[VRSP -136]} 0x40054e = {VRSP
          * -124=[VRDI]} 0x400551 = {VRSP -136=[VRSI]} 0x400555 = {VFS +40=[V(VFS+40)],
-         * RAX=[V(VFS+40)]} -------del----- 0x40055e = {VRSP -16=[V(VFS+40)]} 0x400562 =
+         * RAX=[V(VFS+40)]} -------ignore----- 0x40055e = {VRSP -16=[V(VFS+40)]} 0x400562 =
          * {RAX=[0]} 0x400564 = {VRSP -108=[0]} 0x40056d = {RAX=[0, 1, 2, 3, VINF], VRSP
          * -108=[0, 1, 2, 3, VINF]} 0x400572 = {RDX=[0, 1, 2, 3, VINF], VRSP -108=[0, 1,
          * 2, 3, VINF]} 0x400575 = {VRSP -104=[0], VRSP -92=[3], VRSP -100=[1], VRSP
          * -96=[2]} 0x400579 = {VRSP -108=[1, 2, 3, VINF, 4]} 0x40057d = {VRSP -108=[0,
-         * 1, 2, 3, VINF]} 0x400583 = {RAX=[0x0]} -------del----- 0x400588 =
-         * {RCX=[V(VFS+40)], VRSP -16=[V(VFS+40)]} -------del----- 0x40058c = {VFS
-         * +40=[V(VFS+40)], RCX=[0]} -------del----- 0x40059c = {RBP=[VRBP], RSP=[VRSP,
-         * VRSP -8]} -------del----- 0x40059d = {RSP=[VRSP +8]} -------del-----
+         * 1, 2, 3, VINF]} 0x400583 = {RAX=[0x0]} -------ignore----- 0x400588 =
+         * {RCX=[V(VFS+40)], VRSP -16=[V(VFS+40)]} -------ignore----- 0x40058c = {VFS
+         * +40=[V(VFS+40)], RCX=[0]} -------ignore----- 0x40059c = {RBP=[VRBP], RSP=[VRSP,
+         * VRSP -8]} -------ignore----- 0x40059d = {RSP=[VRSP +8]} -------ignore-----
          */
 
         mapSMAT = new HashMap<>();
@@ -106,93 +106,16 @@ class TestClass {
         mapVS.put("VRSP -108", setVS);
         mapSMAT.put(0x40057dL, mapVS);
 
-        List arrIdx = new ArrayList();
-        long minaddr = 0;
+        ArrayInfer infer = new ArrayInfer();
+        Set<Map<String, List<Long>>> arrayAccess = infer.findArrayAccess(mapSMAT);
 
-        Set<String> nonaddridxset = new HashSet<>();
+        System.out.println(arrayAccess.toString());
 
-        for (Map.Entry<Long, Map<String, Set<String>>> entMapSMAT : mapSMAT.entrySet()) {
-            Long l = entMapSMAT.getKey();
-            mapVS = entMapSMAT.getValue();
-            if (mapVS.size() < 3) { // WIDENING_THRESHOLD == 4
-                /* all stack address accessed by other instructions */
-                for (Map.Entry<String, Set<String>> entMapVS : mapVS.entrySet()) {
-                    String addr = entMapVS.getKey();
-                    if (!addr.contains("VRSP")) {
-                        /* On stack address space ? */
-                        continue;
-                    } else {
-                        nonaddridxset.add(addr);
-                    }
-                }
+        Map<String, List<Long>> scopeAccess = infer.findScopeAccesssWOArray(mapSMAT);
 
-                continue;
-            }
+        System.out.println(scopeAccess.toString());
+         
 
-            arrIdx.clear();
-            System.out.println(mapVS.toString());
-            for (Map.Entry<String, Set<String>> entMapVS : mapVS.entrySet()) {
-                String addr = entMapVS.getKey();
-                if (!addr.contains("VRSP")) {
-                    /* On stack address space ? */
-                    break;
-                }
-
-                /* Stack memory access */
-                String delta = calc.symbolicSub("VRSP", addr);
-                if (!calc.isPureDigital(delta)) {
-                    break;
-                }
-
-                String msg = String.format("Access stack array: VRSP-%d", Long.decode(delta));
-                System.out.println(msg);
-
-                arrIdx.add(Long.decode(delta));
-
-            }
-            if (arrIdx.size() > 0) {
-                Collections.sort(arrIdx);
-                System.out.println(arrIdx.toString());
-
-                long max = (long) Collections.max(arrIdx);
-                minaddr = (long) Collections.min(arrIdx);
-                String msg = String.format("Array base address: VRSP-%d", max);
-                System.out.println(msg);
-
-                long stride = (long) arrIdx.get(1) - (long) arrIdx.get(0);
-                msg = String.format("Array stride is: %d", stride);
-                System.out.println(msg);
-
-            }
-
-        }
-
-        List nonarrIdx = new ArrayList();
-
-        for (String addr : nonaddridxset) {
-            /* Stack memory access */
-            String delta = calc.symbolicSub("VRSP", addr);
-            if (!calc.isPureDigital(delta)) {
-                break;
-            } else {
-                nonarrIdx.add(Long.decode(delta));
-            }
-        }
-        Collections.sort(nonarrIdx);
-
-        /* find max lowerbound */
-        long value = 0;
-        for (int i = 0; i < nonarrIdx.size(); i++) {
-            if ((long) nonarrIdx.get(i) < minaddr) {
-                value = (long) nonarrIdx.get(i);
-            }
-        }
-
-        String msg = String.format("Array lowbound is: %d", value);
-        System.out.println(msg);
-
-        // System.out.println(mapSMAT.toString());
-        System.out.println("Run doTest successfully");
     }
 }
 
